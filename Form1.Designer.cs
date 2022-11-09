b537d3513c0625f66be4a28000f8d8699e114995
harmonashleychatt <102543767+harmonashleychatt@users.noreply.github.com>
2022-11-09 18:42:13 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace harmon_ashley_assignment14
+{
+    partial class DiceGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDie1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDie2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDoubles = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDie3 = new System.Windows.Forms.TextBox();
+            this.buttonRollDice = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTriples = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxScore = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTotalScore = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxNumRolls = new System.Windows.Forms.TextBox();
+            this.Bonus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDie1
+            // 
+            this.textBoxDie1.Location = new System.Drawing.Point(89, 82);
+            this.textBoxDie1.Name = "textBoxDie1";
+            this.textBoxDie1.Size = new System.Drawing.Size(26, 20);
+            this.textBoxDie1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Die 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Die 2";
+            // 
+            // textBoxDie2
+            // 
+            this.textBoxDie2.Location = new System.Drawing.Point(160, 82);
+            this.textBoxDie2.Name = "textBoxDie2";
+            this.textBoxDie2.Size = new System.Drawing.Size(26, 20);
+            this.textBoxDie2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Die 3";
+            // 
+            // textBoxDoubles
+            // 
+            this.textBoxDoubles.Location = new System.Drawing.Point(89, 178);
+            this.textBoxDoubles.Name = "textBoxDoubles";
+            this.textBoxDoubles.Size = new System.Drawing.Size(26, 20);
+            this.textBoxDoubles.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Doubles";
+            // 
+            // textBoxDie3
+            // 
+            this.textBoxDie3.Location = new System.Drawing.Point(231, 82);
+            this.textBoxDie3.Name = "textBoxDie3";
+            this.textBoxDie3.Size = new System.Drawing.Size(26, 20);
+            this.textBoxDie3.TabIndex = 6;
+            // 
+            // buttonRollDice
+            // 
+            this.buttonRollDice.Location = new System.Drawing.Point(135, 151);
+            this.buttonRollDice.Name = "buttonRollDice";
+            this.buttonRollDice.Size = new System.Drawing.Size(75, 23);
+            this.buttonRollDice.TabIndex = 8;
+            this.buttonRollDice.Text = "Roll Dice";
+            this.buttonRollDice.UseVisualStyleBackColor = true;
+            this.buttonRollDice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(228, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Triples";
+            // 
+            // textBoxTriples
+            // 
+            this.textBoxTriples.Location = new System.Drawing.Point(231, 178);
+            this.textBoxTriples.Name = "textBoxTriples";
+            this.textBoxTriples.Size = new System.Drawing.Size(26, 20);
+            this.textBoxTriples.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(154, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Score";
+            // 
+            // textBoxScore
+            // 
+            this.textBoxScore.Location = new System.Drawing.Point(146, 210);
+            this.textBoxScore.Name = "textBoxScore";
+            this.textBoxScore.Size = new System.Drawing.Size(59, 20);
+            this.textBoxScore.TabIndex = 11;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(135, 408);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 13;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label7.Location = new System.Drawing.Point(143, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Total Score";
+            // 
+            // textBoxTotalScore
+            // 
+            this.textBoxTotalScore.Location = new System.Drawing.Point(146, 264);
+            this.textBoxTotalScore.Name = "textBoxTotalScore";
+            this.textBoxTotalScore.Size = new System.Drawing.Size(59, 20);
+            this.textBoxTotalScore.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label8.Location = new System.Drawing.Point(132, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Number of Rolls:";
+            // 
+            // textBoxNumRolls
+            // 
+            this.textBoxNumRolls.Location = new System.Drawing.Point(146, 322);
+            this.textBoxNumRolls.Name = "textBoxNumRolls";
+            this.textBoxNumRolls.Size = new System.Drawing.Size(59, 20);
+            this.textBoxNumRolls.TabIndex = 16;
+            // 
+            // Bonus
+            // 
+            this.Bonus.AutoSize = true;
+            this.Bonus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bonus.ForeColor = System.Drawing.Color.DarkRed;
+            this.Bonus.Location = new System.Drawing.Point(143, 367);
+            this.Bonus.Name = "Bonus";
+            this.Bonus.Size = new System.Drawing.Size(0, 13);
+            this.Bonus.TabIndex = 18;
+            // 
+            // DiceGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 443);
+            this.Controls.Add(this.Bonus);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxNumRolls);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxTotalScore);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxScore);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxTriples);
+            this.Controls.Add(this.buttonRollDice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDie3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDoubles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxDie2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDie1);
+            this.Name = "DiceGame";
+            this.Text = "Dice Game";
+            this.Load += new System.EventHandler(this.DiceGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDie1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDie2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDoubles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDie3;
+        private System.Windows.Forms.Button buttonRollDice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTriples;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxScore;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTotalScore;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxNumRolls;
+        private System.Windows.Forms.Label Bonus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace harmon_ashley_assignment14
+{
+    public partial class DiceGame : Form
+    {
+        public int dob = 0;
+        public int tri = 0;
+        public int score = 0;
+        public int totalscore = 0;
+        public int rollcount = 0;
+
+        public DiceGame()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int x;
+            score = 0;
+            x = rd.Next(1, 7);
+            this.textBoxDie1.Text = x.ToString();
+            score += x;
+            x = rd.Next(1, 7);
+            this.textBoxDie2.Text = x.ToString();
+            score += x;
+            x = rd.Next(1, 7);
+            this.textBoxDie3.Text = x.ToString();
+            score += x;
+            // count doubles and triples
+            if (this.textBoxDie1.Text == this.textBoxDie2.Text && this.textBoxDie1.Text == this.textBoxDie3.Text)
+            {
+                score = score + 50;
+                tri++;
+                this.textBoxTriples.Text = tri.ToString();
+                this.Bonus.Text = "Tripples Bonus $50.00";
+            }
+            else if (this.textBoxDie1.Text == this.textBoxDie2.Text ||
+                this.textBoxDie1.Text == this.textBoxDie3.Text ||
+                this.textBoxDie3.Text == this.textBoxDie2.Text)
+            {
+                score = score + 25;
+                dob++;
+                this.textBoxDoubles.Text = dob.ToString();
+                this.Bonus.Text = "Doubles Bonus $25.00";
+            }
+            else
+                this.Bonus.Text = "No Bonus";
+            // populate score and total score text boxes
+            this.textBoxScore.Text = score.ToString();
+            totalscore = totalscore + score;
+            this.textBoxTotalScore.Text = totalscore.ToString();
+            rollcount++;
+            this.textBoxNumRolls.Text = rollcount.ToString();
+        }
+
+        private void DiceGame_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
